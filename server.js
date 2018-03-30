0940b3b14552fc1ecc85f6440d4686afffdd6257
MASteward <msteward05@hotmail.com>
2018-03-30 00:29:42 -0500

    updated server with mongoDB connection

--- server.js
+++ server.js
@@ -15,11 +15,9 @@ app.use(routes);
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/savedArticles",
-  {
-    useMongoClient: true
-  }
+var MONGODB_URI= process.env.MONGODB_URI || "mongodb://localhost/savedArticles",
+ mongoose.connect(MONGODB_URI);
+  useMongoClient: true
 );
 
 // Start the API server
